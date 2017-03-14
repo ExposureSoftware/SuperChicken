1a159281a8e71acf22ed9d5fcbd02926b60e5146
Marshall Davis <toothlessrebel@gmail.com>
2017-03-14 03:19:24 -0400

    Adds specific exceptions

---
+++ src/Exceptions/Necromancy.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ToothlessRebel
+ * Date: 2017-03-13
+ * Time: 23:57
+ */
+
+namespace ExposureSoftware\SuperChicken\Exceptions;
+
+
+class Necromancy extends \Exception
+{
+
+}
